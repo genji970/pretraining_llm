c12fc34677ae7e4fb17413a4b98912de57299364
genji970 <qnckdrb11@naver.com>
2026-08-06 13:08:35 +0000

    update

--- model/moe.py
+++ model/moe.py
@@ -66,19 +66,57 @@ class SparseMoE(nn.Module):
         router_prob: torch.Tensor,
         selected_experts: torch.Tensor,
     ) -> tuple[torch.Tensor, torch.Tensor]:
-    """
-    router_prob: [total_tokens, num_experts]
-    selected_experts: [total_tokens, top_k]
-    """
-    assignment = F.one_hot(
-        selected_experts,
-        num_classes=self.num_experts,
-    ).float() #assignment.shape : [total_tokens,top_k,num_experts]
+        """
+        router_prob: [total_tokens, num_experts]
+        selected_experts: [total_tokens, top_k]
+        """
+        assignment = F.one_hot(
+            selected_experts,
+            num_classes=self.num_experts,
+        ).float() #assignment.shape : [total_tokens,top_k,num_experts]
 
-    tokens_per_expert = assignment.mean(dim=(0,1))
-    mean_router_prob=router_prob.mean(dim=0)
+        tokens_per_expert = assignment.mean(dim=(0,1))
+        mean_router_prob=router_prob.mean(dim=0)
 
-    auxiliary_loss=self.num_experts*torch.sum(
-        tokens_per_expert*mean_router_prob
-    )
-    return auxilira_loss,tokens_per_expert
+        auxiliary_loss=self.num_experts*torch.sum(
+            tokens_per_expert*mean_router_prob
+        )
+        return auxiliary_loss,tokens_per_expert
+    
+    def forward(
+        self,
+        x: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor, dict[str, torch.Tensor]]
+
+    """
+    x: [batch_size,sequence_length,embed_dim]
+    """
+        batch_size, sequence_length, embed_dim = x.shape
+        if embed_dim != self.embed_dim:
+            raise ValueError(
+                f"expected embed_dim={self.embed_dim},"
+                f"received embed_dim={embed_dim}"
+            )
+        
+        x_flatten=x.reshape(-1,embed_dim)
+        #[total_tokens,num_experts]
+        router_logits=self.router(x_flatten)
+
+        router_prob=torch.softmax(router_logits.float(),dim=-1,)
+
+        # [total_tokens,top_k]
+        router_weight, selected_experts=torch.topk(
+            router_prob,
+            k=self.top_k,
+            dim=-1,
+        )
+
+        routing_weight=routing_weight/routing_weight.sum(
+            dim=-1,
+            keepdim=True,
+        ).to(x_flatten.dtype)
+
+        output=torch.zeros_like(x_flatten)
+
+        
+
